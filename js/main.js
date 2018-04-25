1c8d1f49eeb67e788ecc2caa5e770321ace43b21
niyati shah <niyatishah@niyatis-MacBook-Pro.local>
2018-04-24 17:15:55 -0700

    Javascript updated

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-alert("Hello world");
+//User enters a random letter
+// This function is run whenever the user presses a key.
+
+for( var counter=0; counter<10;counter++){
+    document.onkeyup = function (event) {
+        // Determines which key was pressed.
+        var userGuess = event.key;
+        console.log("userGuess: ", userGuess);
+        document.getElementById("guesses").innerHTML = userGuess;
+    
+        //Computer generates a random letter
+        var randomNumber = Math.floor(Math.random() * 26) + 65;
+    
+    
+        var res = String.fromCharCode(randomNumber);
+        console.log("res:", res);
+    
+        //User guess and computer letter are compared
+    
+        if (res === userGuess) {
+            console.log("your guess is correct");
+        } else {
+            console.log("Incorrect guess! Try again.");
+            counter--;
+        }
+        console.log(counter);
+    }
+}
+
