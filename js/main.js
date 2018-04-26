419e6bdd923806cbb0c41405e7e7d6902fa006f3
niyati shah <niyatishah@niyatis-MacBook-Pro.local>
2018-04-25 21:48:00 -0700

    JS updated

--- js/main.js
+++ js/main.js
@@ -1,29 +1,29 @@
-//User enters a random letter
-// This function is run whenever the user presses a key.
+var i = 0;
 
-for( var counter=0; counter<10;counter++){
+function psychicGame() {
     document.onkeyup = function (event) {
         // Determines which key was pressed.
         var userGuess = event.key;
-        console.log("userGuess: ", userGuess);
-        document.getElementById("guesses").innerHTML = userGuess;
-    
-        //Computer generates a random letter
+        console.log("USER TYPED: ", userGuess);
         var randomNumber = Math.floor(Math.random() * 26) + 65;
-    
-    
         var res = String.fromCharCode(randomNumber);
-        console.log("res:", res);
-    
-        //User guess and computer letter are compared
-    
+        console.log("COMPUTER GUESSED:", res); 67
+        //User guess and computer letter are compare.
         if (res === userGuess) {
             console.log("your guess is correct");
+            return true; //used to break a function
         } else {
             console.log("Incorrect guess! Try again.");
-            counter--;
+            i++;
+            if(i===10){
+                return false; //used to break a function
+            }
         }
-        console.log(counter);
+        console.log("COUNTER: ", i);
+        console.log("-----------------------------");
+        
     }
+    
+   
 }
 
