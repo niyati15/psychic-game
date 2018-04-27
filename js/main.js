112dbe6f663b31aaadf237ddce875b926f3e01c4
niyati shah <niyatishah@niyatis-MacBook-Pro.local>
2018-04-26 22:56:13 -0700

    random number generation and reset logic added

--- js/main.js
+++ js/main.js
@@ -1,58 +1,50 @@
 
 var guesses = [];
-var counter = 0;
+var counter = 1;
+var wins = 0;
+var losses = 0;
 
 var randomNumber = Math.floor(Math.random() * 26) + 65;
 var res = String.fromCharCode(randomNumber);
 console.log("COMPUTER GUESSED:", res);
 
-
-// function random_character() {
-//     var chars = "ABCDEFGHIJKLMNOPQURSTUVWXYZ";
-//     var res= chars.substr( Math.floor(Math.random() * 62), 1);
-//     return res;
-// }
-
 document.onkeyup = function (event) {
     // Determines which key was pressed.
-    var userGuess = event.key;
+    var userGuess = event.key.toUpperCase();
+    // userGuess.toUpperCase();
     console.log("USER TYPED: ", userGuess);
     guesses.push(userGuess);
     document.getElementById("guesses").innerHTML = guesses;
-
     checkGuess(userGuess);
 }
 
 
 function checkGuess(userGuess) {
     if (counter < 10) {
-        counter++;
         document.getElementById("guesses-left").innerHTML = counter;
         if (userGuess === res) {
+            wins++;
             console.log("Success");
+            document.getElementById("wins").innerHTML = wins;
             clearFields();
         }
         else {
             console.log("try again!");
         }
+        counter++;
     } else if (counter === 10) {
         console.log("out of luck!!");
+        losses++;
+        document.getElementById("losses").innerHTML = losses;
         clearFields();
     }
     return false;
 }
 function clearFields() {
-    //reset guess value
-    // guessInput.value = "";
-    //reset counter variable to 0
-    counter = 0;
-    //set randomNumber to be a new random number
-
-    // randomNumber = Math.floor(Math.random() * 26) + 65;
-    // var res = String.fromCharCode(randomNumber);
-
-
-
-    //added to put function out of call stack
+    counter = 1;
+    guesses = [];
+    randomNumber = Math.floor(Math.random() * 26) + 65;
+    res = String.fromCharCode(randomNumber);
+    console.log("COMPUTER GUESSED AGAIN:", res);
     return false;
 }
