f76f1e8da26ce27050eaf80d7a50c37270c75446
niyati shah <niyatishah@niyatis-MacBook-Pro.local>
2018-04-26 21:15:11 -0700

    adding more JS logic

--- js/main.js
+++ js/main.js
@@ -1,29 +1,40 @@
 var i = 0;
-
+var wins = 0;
+var loss = 0;
+var guesses = [];
 function psychicGame() {
     document.onkeyup = function (event) {
         // Determines which key was pressed.
         var userGuess = event.key;
         console.log("USER TYPED: ", userGuess);
+        guesses.push(userGuess);
+        document.getElementById("guesses").innerHTML = guesses;
         var randomNumber = Math.floor(Math.random() * 26) + 65;
         var res = String.fromCharCode(randomNumber);
         console.log("COMPUTER GUESSED:", res); 67
         //User guess and computer letter are compare.
         if (res === userGuess) {
             console.log("your guess is correct");
-            return true; //used to break a function
+            wins++;
+            console.log("Wins: ",wins);
+            document.getElementById("wins").innerHTML = wins;
+            reset();
+            // return;
         } else {
             console.log("Incorrect guess! Try again.");
             i++;
             if(i===10){
-                return false; //used to break a function
+                // lost();
+                loss++;
+                document.getElementById("losses").innerHTML = loss;
+                reset();
             }
         }
-        console.log("COUNTER: ", i);
-        console.log("-----------------------------");
-        
-    }
-    
-   
+    }  
 }
+function reset(){
+    wins = 0;
+    loss = 0;
+    i=0;
 
+}
